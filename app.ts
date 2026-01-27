74c64c6ad35f1fc914ae2d6f62251efc9292ed16
davidmthompson <davidthompson25@proton.me>
2026-01-27 10:43:25 -0700

    Add Of Backend

---
+++ app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express()
+
+app.get('/', (request, response) => {
+     response.send("Welcome to the quote sever")
+})
+
+app.get('/getDailyJoke', (request, response) => {
+     response.send({
+        title: "Our fist Quote",
+        message: "lorem ipsum dolar emit",
+        author: "ME!",
+     })
+})
+app.listen(3000, () => {
+    console.log(`Server is Listening on 3000`)
+}) 
